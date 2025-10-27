c3c7bf88fb6fcf27908db9a5446acea8345839f5
Mikado <mikado@localhost.local>
2025-10-27 12:49:22 +0100

    restructure includes

---
+++ src/pilight/libs/pilight/protocols/protocol_variables.c
@@ -0,0 +1,61 @@
+/*
+  Auto-generiert durch fix-protocol-pointers.ps1
+  Enthält genau eine Definition pro protocol_t * Pointer.
+  Erzeugt: 2025-10-27 11:37:35
+*/
+#include "protocol.h"
+
+struct protocol_t *alecto_ws1700 = NULL;
+struct protocol_t *alecto_wsd17 = NULL;
+struct protocol_t *alecto_wx500 = NULL;
+struct protocol_t *arctech_contact = NULL;
+struct protocol_t *arctech_dimmer = NULL;
+struct protocol_t *arctech_dusk = NULL;
+struct protocol_t *arctech_motion = NULL;
+struct protocol_t *arctech_screen = NULL;
+struct protocol_t *arctech_screen_old = NULL;
+struct protocol_t *arctech_switch = NULL;
+struct protocol_t *arctech_switch_old = NULL;
+struct protocol_t *auriol = NULL;
+struct protocol_t *beamish_switch = NULL;
+struct protocol_t *clarus_switch = NULL;
+struct protocol_t *cleverwatts = NULL;
+struct protocol_t *conrad_rsl_contact = NULL;
+struct protocol_t *conrad_rsl_switch = NULL;
+struct protocol_t *daycom = NULL;
+struct protocol_t *ehome = NULL;
+struct protocol_t *elro_300_switch = NULL;
+struct protocol_t *elro_400_switch = NULL;
+struct protocol_t *elro_800_contact = NULL;
+struct protocol_t *elro_800_switch = NULL;
+struct protocol_t *eurodomest_switch = NULL;
+struct protocol_t *ev1527 = NULL;
+struct protocol_t *heitech = NULL;
+struct protocol_t *impuls = NULL;
+struct protocol_t *iwds07 = NULL;
+struct protocol_t *kerui_D026 = NULL;
+struct protocol_t *logilink_switch = NULL;
+struct protocol_t *mumbi = NULL;
+struct protocol_t *nexus = NULL;
+struct protocol_t *ninjablocks_weather = NULL;
+struct protocol_t *pollin = NULL;
+struct protocol_t *quigg_gt1000 = NULL;
+struct protocol_t *quigg_gt7000 = NULL;
+struct protocol_t *quigg_gt9000 = NULL;
+struct protocol_t *quigg_screen = NULL;
+struct protocol_t *rc101 = NULL;
+struct protocol_t *rsl366 = NULL;
+struct protocol_t *sc2262 = NULL;
+struct protocol_t *secudo_smoke = NULL;
+struct protocol_t *selectremote = NULL;
+struct protocol_t *silvercrest = NULL;
+struct protocol_t *smartwares_switch = NULL;
+struct protocol_t *tcm = NULL;
+struct protocol_t *techlico_switch = NULL;
+struct protocol_t *teknihall = NULL;
+struct protocol_t *tfa = NULL;
+struct protocol_t *tfa2017 = NULL;
+struct protocol_t *tfa30 = NULL;
+struct protocol_t *x10 = NULL;
+
+// EDIT TEST: Schreibbarkeit geprüft
